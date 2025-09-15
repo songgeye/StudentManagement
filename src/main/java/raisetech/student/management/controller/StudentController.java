5c3b85af4a92eaa2772d1f01acf273929572279b
songgeye <79154673+songgeye@users.noreply.github.com>
2025-09-16 01:11:06 +0900

    Update StudentController.java
    コメントアウトを追記

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -43,7 +43,7 @@ public class StudentController {
       studentDetail.setStudent(student);
       for (StudentCourse studentCourse : studentCourses) {
         if (student.getId().equals(studentCourse.getStudentId())) {
-
+          // 処理内容を記載
         }
       }
     }
