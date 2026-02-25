a14c73d0185e2c4963785f4f05b821d75aaa5dfd
songgeye <nov.twenty1120dibali@gmail.com>
2026-02-25 23:06:24 +0900

    ID型をStringからLongへ変更(関連テスト修ya含む)

---
+++ src/main/java/raisetech/student/management/repository/typehandler/ApplicationStatusTypeHandler.java
@@ -0,0 +1,5 @@
+package raisetech.student.management.repository.typehandler;
+
+public class ApplicationStatusTypeHandler {
+
+}
--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,25 +1,29 @@
 CREATE TABLE students (
-  id int NOT NULL AUTO_INCREMENT,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL,
   kana_name VARCHAR(50) NOT NULL,
   nickname VARCHAR(50),
   email VARCHAR(50) NOT NULL,
   area VARCHAR(50) NOT NULL,
   age INT,
-  gender enum('M','F','O') DEFAULT NULL,
+  gender ENUM('M','F','O') DEFAULT NULL COMMENT 'M:男性, F:女性, O:その他/未回答',
   remark VARCHAR(255),
   is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
   PRIMARY KEY (id),
   UNIQUE KEY email (email)
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE students_courses (
-  id int NOT NULL AUTO_INCREMENT,
-  student_id int NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  student_id BIGINT NOT NULL,
   course_name VARCHAR(50) NOT NULL,
   course_start_at TIMESTAMP NULL,
   course_end_at TIMESTAMP NULL,
   application_status VARCHAR(20) NOT NULL DEFAULT 'TEMPORARY',
   PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_0900_ai_ci;
